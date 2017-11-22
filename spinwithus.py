3dc2fe666e6b835dd21c793ecfdb2c8c62ba7305
Ti Nguyen <compdude22@gmail.com>
2017-11-22 08:58:42 -0800

    Aiden, Logan: You must remember to commit frequently. -Ti

--- spinwithus.py
+++ spinwithus.py
@@ -9,31 +9,13 @@ import random
 
 app = Flask(__name__)
 
-message_array = ["Somebody tell Cody to stop frowning.","What's that caribou doing back there?",
-                 "OMG, is that a Pokemon quilt?!", "Is this thing actually updating?",
-                 "Nightmode is 2spooky4me", "What the hell is this, Aiden?", "Does this have a point?",
-                 "I can tell this is some deep social experiment", "Why is Stone always on the couch?",
-                 "Roseville looks like a cool place to live.", "Great job!", "This is dope AF.",
-                 "When will something exciting happen?", "Is this your new startup project?",
-                 "Clean ur house, bro.", "This prevents home invasions.", "What a time to be alive!",
-                 "Truly a masterpiece.", "I could watch this all day!", "This is super weird.",
-                 "I can't even this site.", "I know sites. I have the best sites.", "'Sup?",
-                 "Spot the kitty.", "'I do not denigrate those with a penchant for self-improvement.' -Ti",
-                 "'Have I mentioned the trappings of material wealth?' -Ti" ]
-
-
 @app.route('/')
 def index():
-    if len(message_array) == 0:
-        funnytext = "test"
-    else:
-        funnytext = random.choice(message_array)
-        funnytext = '"%s"' % funnytext
-
     if len(adj_list) == 0:
         adj = "test"
     else:
         adj = random.choice(adj_list)
+    funnytext="DELETE ME"
 
     return render_template('index.html', funnytext=funnytext, adj=adj, header=header, footer=footer)
 
